aefff5d70d74f4b7bd88ec9d21a7409be82da42d
Mustapha Mohammed <mustapha.mohammedu12@gmail.com>
2025-11-14 22:52:22 +0000

    connect db config & complete envjs

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,6 +2,18 @@ import "dotenv/config";
 
 export const ENV = {
   PORT: process.env.PORT || 5001,
+  NODE_ENV: process.env.NODE_ENV,
+
   MONGO_URI: process.env.MONGO_URI,
-  NODE_ENVIRONMENT: process.env.NODE_ENV,
+
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRETE: process.env.STREAM_API_SECRETE,
+
+  SENTRY_DSN: process.env.SENTRY_DSN,
+
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY
 };
